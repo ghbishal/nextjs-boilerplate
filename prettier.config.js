1c0068bc32cf17b45ac5a3984ae62028edda8dcd
bishal_khadka <bishal_khadka@reazon.jp>
2023-08-31 19:11:06 +0900

    chor: add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
+  singleQuote: true,
+  trailingComma: "es5",
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  parser: "typescript",
+};
