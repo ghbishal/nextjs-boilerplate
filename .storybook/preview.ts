77c70f8254d114496879f247411bc4cc49ca35fd
ghbishal <bishalk2056@gmail.com>
2023-10-02 19:11:42 +0900

    fix: remove css import

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,9 +3,6 @@ import type { Preview } from '@storybook/react';
 
 import { withThemeByClassName } from '@storybook/addon-styling';
 
-/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
-import '../src/index.css';
-
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
