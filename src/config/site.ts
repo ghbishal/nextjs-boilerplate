44fd14110b796bdffc33783745939f8e3d8b1a76
bishal khadka <93113583+ghbishal@users.noreply.github.com>
2023-10-05 22:51:33 +0900

    Feat/env config (#23)
    * feat: created env file
    
    * feat: site config & refactored

---
+++ src/config/site.ts
@@ -0,0 +1,5 @@
+export const SITE = {
+  title: 'Nextjs Starter Boilerplate',
+  description:
+    "🚀 Explore the ultimate Next.js 13+ starter kit! 📦 It's a dynamic blend of technology: Next.js + TypeScript 📜, ESLint ✨, Prettier 🌟, Tailwind CSS 🎨, and more. Tailored for scalability, it even includes GitHub Actions 🤖, VSCode 💻, and Storybook 📖. Get coding with confidence! 🚀💼  Resources",
+} as const;
