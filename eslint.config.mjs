6c137428df796744a8a9e8bf403a908c60f95be1
Bishal Khadka <93113583+ghbishal@users.noreply.github.com>
2025-04-01 21:31:58 +0900

    Dep update remove (#44)
    * dep: updated next-intl to latest
    
    * dep: updated tailwind css to latest
    
    * dep: updated other dep
    
    * dep: update eslint
    
    * dep: update eslint format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import antfu from '@antfu/eslint-config';
 // @ts-ignore
 import nextPlugin from '@next/eslint-plugin-next';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
-import tailwind from 'eslint-plugin-tailwindcss';
 
 export default antfu(
   {
@@ -26,7 +25,6 @@ export default antfu(
       'next-env.d.ts',
     ],
   },
-  ...tailwind.configs['flat/recommended'],
   jsxA11y.flatConfigs.recommended,
   {
     plugins: {
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 import './src/lib/env.mjs';
 
+/* This path is supported by default: ./(src/)i18n/request.{js,jsx,ts,tsx} .
+If you want to use a different path, you can pass a custom path to the plugin:
+*/
 const withNextIntlConfig = createNextIntlPlugin('./src/i18n/i18n.ts');
 
 /** @type {import('next').NextConfig} */
